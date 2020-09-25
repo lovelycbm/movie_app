de31b7d282c76288c2c2001cb09948bfddd13c84
lovelycbm <lovelycbm@naver.com>
2020-09-25 15:56:00 +0900

    last_commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
-  },  
-  {
-    id : 2,
-    name: "samgyetang",
-    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      'https://yts.mx/api/v2/list_movies.json?sort_by=rating',
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
   }
-]
 
-function Food({name, picture}){  
-  return <div>
-    <h2> I like {name}</h2>
-    <img src={picture} alt={name} />
-  </div>
-}
-
-// 컴포넌트 _ html을 반환함.
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key = {dish.id} name={dish.name} picture={dish.image}/>
-        ))}
-    </div>
-  );
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
